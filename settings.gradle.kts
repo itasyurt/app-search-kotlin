a8c57c38417e9afbac01d379a4f8c514569dd63d
Ibrahim Tasyurt <ibrahim.tasyurt@udemy.com>
2020-12-19 22:06:14 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,9 @@
 rootProject.name = "app-search-kotlin"
 
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
+
